916e1ef28259e7b13b367958f0902c06d5922581
NehemiahLimo <nehemiahlimo02@gmail.com>
2021-07-30 18:07:59 +0300

    Added api, apiResources, routes to the app

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,44 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'middle_name',
+        'address',
+        'zip_code',
+        'department_id',
+        'state_id',
+        'city_id',
+        'country_id',
+        'date_hired',
+        'birthdate',
+    ];
+
+    protected $casts = [
+        'birthdate' => 'datetime:Y-m-d',
+
+        'date_hired' => 'datetime:Y-m-d',
+    ];
+
+
+
+    public function country(){
+        return $this->belongsTo(Country::class);
+    }
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    
+
 }
